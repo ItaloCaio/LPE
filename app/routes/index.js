9c4d08dd929efb1d87523b884795caf2e9dff90b
Italo Caio <it-caio@hotmail.com>
2019-06-10 08:39:30 -0300

    testando outra porta

--- app/routes/index.js
+++ app/routes/index.js
@@ -8,10 +8,25 @@ module.exports = function(application){
 	application.get('/sobre', function(req, res){
 		res.render('sobre');
 	});
-	application.get('/', function(req, res){
-		res.render('sobre');
+
+
+
+	application.get('/somar', function(req, res){
+		res.render('somar');
 	});
-	application.get('/', function(req, res){
-		res.render('inicio');
+	application.get('/subtrair', function(req, res){
+		res.render('subtrair');
 	});
+	application.get('/peppa', function(req, res){
+		res.render('peppa');
+	});
+	application.get('/luna', function(req, res){
+		res.render('luna');
+	});
+	application.get('/patrulha', function(req, res){
+		res.render('patrulha');
+	});
+
+
+
 }
